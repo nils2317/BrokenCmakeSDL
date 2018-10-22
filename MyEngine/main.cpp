a13a87869d4f7b961783ee7c9b5577034206c84b
Nils Steinbuegl <nils1723@gmail.com>
2018-10-21 23:43:08 -0400

    Something is off by just a bit...

--- MyEngine/main.cpp
+++ MyEngine/main.cpp
@@ -1,6 +1,20 @@
 #include "Headers/Scene.h"
 
 int main(){
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto *myScene = new Scene();
+
+    myScene->CreateGameObject("Player");
+    myScene->CreateGameObject("Enemy");
+
+    myScene->FindGameObject("Player")->AddComponent<Renderer>();
+    myScene->FindGameObject("Player")->AddComponent<Physics>();
+
+    myScene->FindGameObject("Enemy")->AddComponent<Renderer>();
+
+    myScene->FindGameObject("Player")->Update();
+    myScene->FindGameObject("Enemy")->Update();
+
+    myScene->FindGameObject("Player")->RemoveComponent<Physics>();
+    myScene->FindGameObject("Player")->Update();
+
 }
