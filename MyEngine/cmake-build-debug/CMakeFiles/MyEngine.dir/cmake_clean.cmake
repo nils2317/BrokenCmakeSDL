527380fe2a5c88a2c48b8cdaec93709d8e4aae49
Nils Steinbuegl <nils1723@gmail.com>
2018-10-21 22:25:58 -0400

    Starting Engine

---
+++ MyEngine/cmake-build-debug/CMakeFiles/MyEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyEngine.dir/main.cpp.obj"
+  "MyEngine.pdb"
+  "MyEngine.exe"
+  "MyEngine.exe.manifest"
+  "MyEngine.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
